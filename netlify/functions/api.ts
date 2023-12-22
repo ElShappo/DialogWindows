eb75fbc16bc02e2651b4d4c74f4ea0d79a4ae430
Deni Shaipov <deni.shaipp@gmail.com>
2023-12-22 17:08:50 +0300

    minor

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -10,9 +10,9 @@ const records: POSTBody = []
 
 const router = Router();
 router.get('/hello', (req, res) => res.send('Hello World!'));
-router.get('/get_records', (req, res) => res.send(records));
+router.get('/get_records', (req, res) => res.json(records));
 router.post('/add_records', (req, res) => {
-  const result =  req.body as POSTBody
+  const result = req.body as POSTBody
   for (const record of result) {
     records.push(record)
   }
