b6889b317a221c9ac752ad2b46064487d884b6c2
Deni Shaipov <deni.shaipp@gmail.com>
2023-12-22 18:22:35 +0300

    improved columns definition

--- src/pages/TablePage.vue
+++ src/pages/TablePage.vue
@@ -1,11 +1,27 @@
 <script lang="ts">
-import { POSTBody } from 'src/types';
+import { POSTBody, POSTRecord } from 'src/types';
 import { defineComponent, onMounted, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
   setup() {
-    const columns = ref(['Название окна', 'Название поля', 'Значение поля']);
+    const columns = ref([
+      {
+        name: 'Название окна',
+        label: 'Название окна',
+        field: (row: POSTRecord) => row.title,
+      },
+      {
+        name: 'Название поля',
+        label: 'Название поля',
+        field: (row: POSTRecord) => row.label,
+      },
+      {
+        name: 'Значение поля',
+        label: 'Значение поля',
+        field: (row: POSTRecord) => row.value,
+      },
+    ]);
     const rows = ref<POSTBody>([]);
 
     onMounted(async () => {
